beb6784bb3c04e3bdbdb9127e27a97c14ae72664
Laurent Cozic <laurent@pogopixels.com>
2014-03-18 02:20:35 -0300

    Force update of dependencies

--- Formula/massren.rb
+++ Formula/massren.rb
@@ -2,18 +2,18 @@ require "formula"
 
 class Massren < Formula
   homepage "https://github.com/laurent22/massren"
-  url "https://github.com/laurent22/massren/archive/v1.2.0.tar.gz"
-  sha1 "eba08f70796841efaa302c2325890d5a44eecbe0"
+  url "https://github.com/laurent22/massren/archive/v1.2.1.tar.gz"
+  sha1 "585f9650a99026c883b7f620300a570e592d380e"
   
   depends_on 'go' => :build
 
   def install
     ENV['GOPATH'] = buildpath
-    system 'go', 'get', 'github.com/jessevdk/go-flags'
-    system 'go', 'get', 'github.com/kr/text'
-    system 'go', 'get', 'github.com/laurent22/go-sqlkv'
-    system 'go', 'get', 'github.com/laurent22/go-trash'
-    system 'go', 'get', 'github.com/mattn/go-sqlite3'
+    system 'go', 'get', '-u', 'github.com/jessevdk/go-flags'
+    system 'go', 'get', '-u', 'github.com/kr/text'
+    system 'go', 'get', '-u', 'github.com/laurent22/go-sqlkv'
+    system 'go', 'get', '-u', 'github.com/laurent22/go-trash'
+    system 'go', 'get', '-u', 'github.com/mattn/go-sqlite3'
     system 'go', 'build', '-o', 'massren'
     bin.install 'massren'
   end
