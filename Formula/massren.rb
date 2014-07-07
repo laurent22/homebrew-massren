8846ea3dce6c8b3ccfe02b0f85634de6061eaf7c
Laurent Cozic <laurent@pogopixels.com>
2014-07-06 23:46:10 -0300

    Upgraded for v1.3.0

--- Formula/massren.rb
+++ Formula/massren.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Massren < Formula
   homepage "https://github.com/laurent22/massren"
-  url "https://github.com/laurent22/massren/archive/v1.2.1b.tar.gz"
-  sha1 "8b5939841014c79be51b8e6be40f7f14e94895fa"
-  version "1.2.1b"
+  url "https://github.com/laurent22/massren/archive/v1.3.0.tar.gz"
+  sha1 "175c274570c0c6061f6d2371ab1f32941e113d0f"
+  version "1.3.0"
   
   depends_on 'go' => :build
 
@@ -15,6 +15,7 @@ class Massren < Formula
     system 'go', 'get', '-u', 'github.com/laurent22/go-sqlkv'
     system 'go', 'get', '-u', 'github.com/laurent22/go-trash'
     system 'go', 'get', '-u', 'github.com/mattn/go-sqlite3'
+    system 'go', 'get', '-u', 'github.com/nu7hatch/gouuid'
     system 'go', 'build', '-o', 'massren'
     bin.install 'massren'
   end
